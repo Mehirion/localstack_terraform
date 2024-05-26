46e839bb36c413be4ec20e80b6f21d5dbb3973c2
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 20:51:08 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   alias                      = "localstack"
+  access_key                 = ""
+  secret_key                 = ""
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
