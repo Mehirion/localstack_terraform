f4ffc744ac1bde1c4871c53ad5bcb6c3a79037d1
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 20:20:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
-provider "aws" {
-  access_key = "test"  # Ustaw swój access key
-  secret_key = "test"  # Ustaw swój secret key
-
-  # Opcjonalne - konfiguracja dla LocalStack
-  region = "us-east-1"
-  s3_force_path_style = true
-  skip_credentials_validation = true
-  skip_metadata_api_check = true
-
-  endpoints {
-    s3 = "http://localhost:4566"  # Endpoint dla LocalStack S3
-  }
+resource "aws_s3_bucket" "example" {
+  bucket = "my-test-bucket"
+  acl    = "private"
 }
