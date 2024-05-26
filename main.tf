a67a95e98699fd29f7fece87341aeedd4e3f03bb
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 20:14:51 +0200

    Update1 main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_access_key           = "test"
+  access_key = "test"  # Ustaw swój access key
+  secret_key = "test"  # Ustaw swój secret key
+
+  # Opcjonalne - konfiguracja dla LocalStack
+  region = "us-east-1"
+  s3_force_path_style = true
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  s3_force_path_style         = true
-  endpoint                    = "http://localhost:4566"
-}
+  skip_metadata_api_check = true
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
+  endpoints {
+    s3 = "http://localhost:4566"  # Endpoint dla LocalStack S3
   }
 }
