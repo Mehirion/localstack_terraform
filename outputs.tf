91df08a7356cf205c378dbcb0526f1f95932eaaa
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 20:21:05 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,3 @@
 output "bucket_id" {
-  description = "ID bucketu S3"
-  value       = aws_s3_bucket.example_bucket.id
-}
-
-output "bucket_arn" {
-  description = "ARN bucketu S3"
-  value       = aws_s3_bucket.example_bucket.arn
+  value = aws_s3_bucket.example.id
 }
