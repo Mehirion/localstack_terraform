4dffbfecf31b3adc7bfec5f71078ffeed34c9f24
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 20:37:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 resource "aws_s3_bucket" "example" {
   provider = aws.localstack
   bucket   = var.bucket_name
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  provider = aws.localstack
+  bucket   = aws_s3_bucket.example.id
   acl      = "private"
 }
