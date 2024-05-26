a46e0b921d41a5706a5fe2e17cdd76c5a29f5ab0
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 20:26:42 +0200

    Update10 provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  access_key                  = "test"
-  secret_access_key           = "test"
-  region                      = "us-east-1"
-  s3_force_path_style         = true
+  alias                      = "localstack"
+  access_key                 = "test"
+  secret_access_key          = "test"
+  region                     = "us-east-1"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
   endpoints {
     s3 = "http://localhost:4566"
   }
