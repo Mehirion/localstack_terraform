2fa1f9069b5efd8f8e2e26043afac9706c142d56
Kamil Kowalczyk <Mehirion1@gmail.com>
2024-05-26 21:06:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-  default     = "my-local-bucket"
-}
-
-provider "aws" {
-  alias                      = "localstack"
-  access_key                 = "test"
-  secret_key                 = "test"
-  region                     = "us-east-1"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
-}
-
 resource "aws_s3_bucket" "example" {
   provider = aws.localstack
   bucket   = var.bucket_name
